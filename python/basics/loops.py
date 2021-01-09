2acbc18de79488bbd48157a82783da6ba572f2ad
Marta <marta.nunes.abreu@gmail.com>
2021-01-09 19:07:07 +0000

    added standalone tasks

---
+++ python/basics/loops.py
@@ -0,0 +1,16 @@
+for name in "John", "Sam", "Jill":
+	print("Hello " + name)
+	
+total=0
+for i in 5,7,11,13:
+	print(i)
+	total=total+i
+print(total)
+
+for _ in range(10):
+	print("Hello") 
+	
+list_of_beverages = ["Water", "Iced Tea", "Coca Cola"]
+
+for drink in list_of_beverages:
+	print("Would you like a " + drink + "?")
---
+++ python/basics/variables.py
@@ -0,0 +1,33 @@
+
+# type is a builtin function with returns the type of a value
+x = 3 # class int
+x = 7.0 # class float
+x = True # class bool
+x = '7.0' # class string
+
+# string - second example
+phrase = "hello world"
+print(phrase)
+# different method
+print("hi, it's me")
+
+# boolean
+x = False
+print(x)
+if x:
+    print("it's true")
+
+
+# getting user input
+name = input("Give me your name: ") # always returns a string
+print("Your name is " + name)
+
+
+
+print("I will now count my chickens: ")
+print("Hens", 25+30/6)
+print("Roosters", 100-25 + 3%4)
+print("is it true that 3+2 < 5-7?")
+print(3+2<5-7)
+print("What i 3 +2", 3+2)
+print("is it greater?", 5>-2)
