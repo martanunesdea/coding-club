9925a13c33812ab148d7d0527dbe378e10ac7425
Marta <marta.nunes.abreu@gmail.com>
2021-05-07 07:45:01 +0100

    resolved active items issue

--- tkinter/task2_todo_list.py
+++ tkinter/task2_todo_list.py
@@ -5,9 +5,8 @@ from tkinter import ttk
 class ToDoItem(tk.Frame):
     def __init__(self, parent, str_input, count):
         super().__init__(parent)
-        
         self.is_checked = tk.IntVar()
-        self.todo_button = ttk.Checkbutton(parent, text=str_input, variable=self.is_checked, command= lambda *args: self.kill(parent.active_items))
+        self.todo_button = ttk.Checkbutton(parent, text=str_input, variable=self.is_checked, command = lambda: self.kill(parent))
         self.todo_button.grid(row = count, column = 1, pady = 5)
 
     def is_done(self):
@@ -16,16 +15,16 @@ class ToDoItem(tk.Frame):
         else:
             return False
 
-    def kill(self, active_items):
-        print(active_items)
-        parent.active_items -= 1
+    def kill(self, parent):
+        print("Active items", parent.active_items)
         self.todo_button.destroy()
+        parent.update_total_items(1)
 
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("To Do App")
-        self.geometry("325x500")
+        self.geometry("350x500")
         self.active_items = 0
         self.completed_items = 0
         self.position = 2
@@ -37,37 +36,23 @@ class Application(tk.Tk):
         self.my_entry.grid(row = 0, column = 1, pady = 5)
         
         self.button_enter = ttk.Button(self, text = "Enter", command = self.enter)
-        self.button_enter.grid(row = 1, column = 0, columnspan=2,padx = 5, pady = 5)
-
-        self.button_update = ttk.Button(self, text = "Update", command = self.update)
-        self.button_update.grid(row = 1, column = 2, padx = 5, pady = 5)
+        self.button_enter.grid(row = 1, column = 1, padx = 5, pady = 5)
 
         self.button_summary = ttk.Button(self, text = "Summary", command = self.summary)
-        self.button_summary.grid(row = 10, column = 1, padx = 5, pady = 5)
+        self.button_summary.grid(row = 15, column = 1, padx = 5, pady = 5)
 
-        self.status = tk.StringVar()
-        self.statusbar = ttk.Label(self, textvariable=self.status)
-        self.statusbar.grid(row = 1, column = 1)
 
     def enter(self):
         self.todo_collection.append(ToDoItem(self, self.my_entry.get(), self.position))
         self.active_items = self.active_items + 1
         self.position += 1
         print(str(len(self.todo_collection)), " active items ", self.active_items)
-    
-    def update(self):
-        for item in self.todo_collection:
-            if item.is_done():
-                item.kill()
-                self.active_items = self.active_items - 1
-                self.completed_items = self.completed_items + 1
-                print(str(len(self.todo_collection)), " active items ", self.active_items)
-                self.todo_collection.remove(item)
-                continue
 
-    
+    def update_total_items(self, number):
+        self.active_items = self.active_items - 1
+        self.completed_items = self.completed_items + 1
+
     def summary(self):
-        self.update()
         if self.completed_items > 0:
             print("You completed", self.completed_items, "tasks today. Well done!")
         else: 
